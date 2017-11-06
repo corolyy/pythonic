353f20090458c9369e7d8ffe0ba101bc5dd89b83
corolyy <coroyu@163.com>
2017-11-06 23:27:56 +0800

    Initial

---
+++ src/metaclass_attribute/property_decorator.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from datetime import timedelta
+
+
+class Register(object):
+    def __init__(self, ohms):
+        self.ohms = ohms
+        self.voltage = 0
+        self.current = 0
+
+"""Rule of least surprise"""
+class Bucket(object):
+    def __init__(self, period):
+        self.period_delta = timedelta(seconds=period)
+        self.reset_time = datetime.now()
+        self.quota = 0
+
+    def __repr__(self):
+        return 'Bucket(quota=%d)' % self.quota
+
+
+class ProBucket(object):
+    def __init__(self, period):
+        self.period_delta = timedelta(seconds=period)
+        self.reset_time = datetime.now()
+        self.max_quota = 0
+        self.quota_consumed = 0
+
+    def __repr__(self):
+        return ('Bucket(max_quota=%d, quota_consumed=%d)' %
+                (self.max_quota, self.quota_consumed))
+
+    @property
+    def quota(self):
+        return self.max_quota - self.quota_consumed
+
+    @quota.setter
+    def quota(self, amount):
+        delta = self.max_quota - amount
+        if amount == 0:
+            # Quota being reset for a new period
+            self.quota_consumed = 0
+            self.max_quota = 0
+        elif delta < 0:
+            # Quota being filled for the new period
+            assert self.quota_consumed == 0
+            self.max_quota = amount
+        else:
+            # Quota being consumed during the period
+            assert self.max_quota >= self.quota_consumed
+            self.quota_consumed += delta
+
+
+pro_bucket = ProBucket(60)
+print 'Initial', pro_bucket
